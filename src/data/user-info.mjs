38b92d4307e8a2999c926d11d2c84c957b48be41
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-21 11:34:02 +0530

    add sample data

---
+++ src/data/user-info.mjs
@@ -0,0 +1,18 @@
+export const userInfo = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        email: "sdfsfsf@gmail.com"
+    },
+
+    {
+        id: 3,
+        name: "Alice Johnson",
+        email: "dsfswes@gmail.com"
+    }
+]
