bb334a146a96ea90002c97e546c3f0941964a0ea
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-21 11:01:18 +0530

    push

--- server.mjs
+++ server.mjs
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
 });
 
 
+
+app.get('/api/v1/hello', (req, res) => {
+  res.send({ message: 'Hello, World!' });
+  res.status(200).json({ message: 'Hello, World!' });
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
